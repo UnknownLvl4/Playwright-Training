77871c6fef167913b76fd66acf0c3fc1369bcaff
UnknownLvl4 <hongocduyyu@gmail.com>
2025-08-27 15:27:52 +0700

    Playwright Trace viewer feature code, and tracing zip file

---
+++ PlaywrightSessions/src/main/java/PlaywrightSessions/TracerInspection.java
@@ -0,0 +1,37 @@
+package PlaywrightSessions;
+
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
+
+import java.nio.file.Paths;
+
+public class TracerInspection {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            BrowserContext context = browser.newContext();
+            // Start tracing before creating / navigating a page.
+            context.tracing().start(new Tracing.StartOptions()
+                    .setScreenshots(true)
+                    .setSnapshots(true)
+                    .setSources(true));
+            Page page = context.newPage();
+            page.navigate("https://academy.naveenautomationlabs.com/");
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Login")).click();
+            page.locator("#microfe-popup-login").contentFrame().getByRole(AriaRole.TEXTBOX, new FrameLocator.GetByRoleOptions().setName("Name")).click();
+            page.locator("#microfe-popup-login").contentFrame().getByRole(AriaRole.TEXTBOX, new FrameLocator.GetByRoleOptions().setName("Name")).fill("ngocduy");
+            page.locator("#microfe-popup-login").contentFrame().getByRole(AriaRole.TEXTBOX, new FrameLocator.GetByRoleOptions().setName("Email address")).fill("duy@gmail.com");
+            page.locator("#microfe-popup-login").contentFrame().getByRole(AriaRole.TEXTBOX, new FrameLocator.GetByRoleOptions().setName("Password")).fill("123456789");
+            page.locator("#microfe-popup-login").contentFrame().getByRole(AriaRole.BUTTON, new FrameLocator.GetByRoleOptions().setName("Vietnam: +")).click();
+            page.locator("#microfe-popup-login").contentFrame().getByText("Vietnam").click();
+            page.locator("#microfe-popup-login").contentFrame().getByRole(AriaRole.TEXTBOX, new FrameLocator.GetByRoleOptions().setName("Enter your number")).click();
+            page.locator("#microfe-popup-login").contentFrame().getByRole(AriaRole.TEXTBOX, new FrameLocator.GetByRoleOptions().setName("Enter your number")).fill("+84 012 3456");
+            page.locator("#microfe-popup-login").contentFrame().locator("#loginPopupCloseBtn path").click();
+
+            // Stop tracing and export it into a zip archive.
+            context.tracing().stop(new Tracing.StopOptions()
+                    .setPath(Paths.get("trace.zip")));
+            // Access to this link: https://trace.playwright.dev/ and drop the tracing zip file to open
+        }
+    }
+}
