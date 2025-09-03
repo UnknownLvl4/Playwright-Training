a2553381a6bfe71ba66b1bbfd2c1eca8d1459715
UnknownLvl4 <hongocduyyu@gmail.com>
2025-09-03 23:45:54 +0700

    Create Playwright Visible Elements project

---
+++ PlaywrightSessions/src/main/java/PlaywrightSessions/VisibleElements.java
@@ -0,0 +1,21 @@
+package PlaywrightSessions;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class VisibleElements {
+    public static void main(String[] args) {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Page page = browser.newPage();
+
+        //button:visible
+        //button >> visible=true
+
+        page.navigate("");
+        String text = page.locator("").textContent();
+        System.out.println(text);
+    }
+}
