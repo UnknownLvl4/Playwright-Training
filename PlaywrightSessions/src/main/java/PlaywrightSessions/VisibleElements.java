fe47daddd812c8aa3df8c0a3634b92802612b5e6
UnknownLvl4 <hongocduyyu@gmail.com>
2025-09-04 15:08:56 +0700

    Playwright Visible Elements code

--- PlaywrightSessions/src/main/java/PlaywrightSessions/VisibleElements.java
+++ PlaywrightSessions/src/main/java/PlaywrightSessions/VisibleElements.java
@@ -5,6 +5,8 @@ import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 
+import java.util.List;
+
 public class VisibleElements {
     public static void main(String[] args) {
         Playwright playwright = Playwright.create();
@@ -14,8 +16,23 @@ public class VisibleElements {
         //button:visible
         //button >> visible=true
 
-        page.navigate("");
-        String text = page.locator("").textContent();
-        System.out.println(text);
+//        page.navigate("D:\\Playwright-Training\\PlaywrightSessions\\index.html");
+////        String text = page.locator("button:visible").textContent();
+//        String text = page.locator("button >> visible=true").textContent();
+//        System.out.println(text);
+
+        page.navigate("https://www.amazon.com/");
+        page.locator(".a-button-text").click();
+        page.waitForTimeout(1000);
+
+        List<String> linksText = page.locator("a:visible").allInnerTexts();
+        for (int i = 0; i < linksText.size(); i++) {
+            System.out.println(linksText.get(i));
+        }
+
+        int imagesCount = page.locator("xpath=//img >> visible=true").count();
+        System.out.println(imagesCount);
+
+        //text=Login >> visible=true
     }
 }
