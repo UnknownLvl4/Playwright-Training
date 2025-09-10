6525ebd500565de6a8f2558fa046821a3020ff8f
UnknownLvl4 <hongocduyyu@gmail.com>
2025-09-10 23:05:38 +0700

    Playwright Automatic Login code

---
+++ PlaywrightSessions/src/main/java/PlaywrightSessions/Auth.java
@@ -0,0 +1,19 @@
+package PlaywrightSessions;
+
+import com.microsoft.playwright.*;
+import java.nio.file.Paths;
+
+public class Auth {
+    public static void main(String[] args) {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        BrowserContext brContext = browser.newContext();
+        Page page = brContext.newPage();
+        page.navigate("http://www.automationpractice.pl/");
+        page.click("a:text('Sign in')");
+        page.fill("#email", "duy.hn.63cntt@ntu.edu.vn");
+        page.fill("#passwd", "zzz579314");
+        page.click("#SubmitLogin");
+        brContext.storageState(new BrowserContext.StorageStateOptions().setPath(Paths.get("applogin.json")));
+    }
+}
