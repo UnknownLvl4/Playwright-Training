fa9ee3e9e44287d066af225a7f91c502f5f45aec
UnknownLvl4 <hongocduyyu@gmail.com>
2025-09-13 14:54:06 +0700

    Optimize Hard Coded Data; Add Testing Regression in Playwright POM project

--- PlaywrightPOM/src/main/java/com/qa/opencart/factory/PlaywrightFactory.java
+++ PlaywrightPOM/src/main/java/com/qa/opencart/factory/PlaywrightFactory.java
@@ -2,15 +2,21 @@ package com.qa.opencart.factory;
 
 import com.microsoft.playwright.*;
 import com.microsoft.playwright.BrowserType.LaunchOptions;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
 public class PlaywrightFactory {
     Playwright playwright;
     Browser browser;
     BrowserContext browserContext;
     Page page;
+    Properties prop;
 
-    public Page initBrowser(String browserName) {
-        System.out.println("Browser name : " + browserName);
+    public Page initBrowser(Properties prop) {
+        String browserName = prop.getProperty("browser").trim();
+        System.out.println("Browser name is : " + browserName);
         playwright = Playwright.create();
         switch (browserName.toLowerCase()) {
             case "chromium":
@@ -31,8 +37,24 @@ public class PlaywrightFactory {
         }
         browserContext = browser.newContext();
         page = browserContext.newPage();
-        page.navigate("https://naveenautomationlabs.com/opencart/");
+        page.navigate(prop.getProperty("url").trim());
 
         return page;
+    }
+    /*
+        this method is used to initialize the properties from config file
+     */
+    public Properties init_prop() {
+        try {
+            FileInputStream ip = new FileInputStream("./src/test/resources/config/config.properties");
+            prop = new Properties();
+            prop.load(ip);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return prop;
     }
 }
--- PlaywrightPOM/src/test/java/com/qa/opencart/tests/HomePageTest.java
+++ PlaywrightPOM/src/test/java/com/qa/opencart/tests/HomePageTest.java
@@ -1,6 +1,8 @@
 package com.qa.opencart.tests;
 
 import com.microsoft.playwright.Page;
+import com.qa.opencart.base.BaseTest;
+import com.qa.opencart.constants.AppConstants;
 import com.qa.opencart.factory.PlaywrightFactory;
 import com.qa.opencart.pages.HomePage;
 import org.testng.Assert;
@@ -9,28 +11,17 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-public class HomePageTest {
-    PlaywrightFactory pf;
-    Page page;
-    HomePage homePage;
-
-    @BeforeTest
-    public void setup() {
-        pf = new PlaywrightFactory();
-        page = pf.initBrowser("chromium");
-        homePage = new HomePage(page);
-    }
-
+public class HomePageTest extends BaseTest {
     @Test
     public void homePageTitleTest() {
         String actualTitle = homePage.getHomePageTitle();
-        Assert.assertEquals(actualTitle, "Your Store");
+        Assert.assertEquals(actualTitle, AppConstants.LOGIN_PAGE_TITLE);
     }
 
     @Test
     public void homePageURLTest() {
         String actualURL = homePage.getHomePageURL();
-        Assert.assertEquals(actualURL, "https://naveenautomationlabs.com/opencart/");
+        Assert.assertEquals(actualURL, prop.getProperty("url"));
     }
 
     @DataProvider
@@ -47,9 +38,4 @@ public class HomePageTest {
         String actualSearchHeader = homePage.doSearch(productName);
         Assert.assertEquals(actualSearchHeader, "Search - " + productName);
     }
-
-    @AfterTest
-    public void tearDown() {
-        page.context().browser().close();
-    }
-}   
+}
