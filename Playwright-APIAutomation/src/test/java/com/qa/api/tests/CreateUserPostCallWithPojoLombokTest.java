f851aec17a09e5c37d8cbf916edda095fd2668d4
UnknownLvl4 <hongocduyyu@gmail.com>
2025-09-16 23:04:21 +0700

    Playwright API POST Call using Lombok Library

---
+++ Playwright-APIAutomation/src/test/java/com/qa/api/tests/CreateUserPostCallWithPojoLombokTest.java
@@ -0,0 +1,75 @@
+package com.qa.api.tests;
+
+import com.api.data.User;
+import com.api.data.Users;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microsoft.playwright.APIRequest;
+import com.microsoft.playwright.APIRequestContext;
+import com.microsoft.playwright.APIResponse;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.RequestOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import java.io.IOException;
+
+public class CreateUserPostCallWithPojoLombokTest {
+    Playwright playwright;
+    APIRequest request;
+    APIRequestContext requestContext;
+    static String emailId;
+
+    @BeforeTest
+    public void setup() {
+        playwright = Playwright.create();
+        request = playwright.request();
+        requestContext = request.newContext();
+    }
+
+    @AfterTest
+    public void tearDown() {
+        playwright.close();
+    }
+
+    public static String getRandomEmail() {
+        emailId = "testpwautomation" + System.currentTimeMillis() + "@gmail.com";
+        return emailId;
+    }
+
+    @Test
+    public void createUserTest() throws IOException {
+
+        //Create users object: using builder pattern:
+        Users users = Users.builder()
+                .name("Duy Automation")
+                .email(getRandomEmail())
+                .gender("male")
+                .status("active").build();
+
+        //POST Call: create a user
+        APIResponse apiPostResponse = requestContext.post("https://gorest.co.in/public/v2/users", RequestOptions.create()
+                .setHeader("content-type", "application/json")
+                .setHeader("Authorization", "Bearer 3644af0942c7e1088d725459dd7c4b3afa90decde4e2bb9a867127ce1fb8c2d7")
+                .setData(users));
+
+        System.out.println(apiPostResponse.status());
+        Assert.assertEquals(apiPostResponse.status(), 201);
+        Assert.assertEquals(apiPostResponse.statusText(), "Created");
+
+        String responseText = apiPostResponse.text();
+        System.out.println(responseText);
+
+        //Convert response text/json to POJO -- desrialization
+        ObjectMapper objectMapper = new ObjectMapper();
+        User actUser = objectMapper.readValue(responseText, User.class);
+        System.out.println("actual user from the response---->");
+        System.out.println(actUser);
+
+        Assert.assertEquals(actUser.getName(), users.getName());
+        Assert.assertEquals(actUser.getEmail(), users.getEmail());
+        Assert.assertEquals(actUser.getStatus(), users.getStatus());
+        Assert.assertEquals(actUser.getGender(), users.getGender());
+        Assert.assertNotNull(actUser.getId());
+    }
+}
